cb0d4ead4636c8e455a28fa28c7e8586f0b04f79
ambikarajesh <ambikula@gmail.com>
2019-03-06 22:58:53 -0800

    changed exact click for right arrow

--- public/js/home.js
+++ public/js/home.js
@@ -1,15 +1,15 @@
 const slide = document.querySelector('.main-content_item-list');
 const size = document.querySelector('.main-content_item').clientWidth;
-const total = document.querySelectorAll('.main-content_item').length;
+let total = document.querySelectorAll('.main-content_item').length;
+const pageSize = document.querySelector('.main-content_wrapper').clientWidth;
+total = total - Math.floor(pageSize/size);
 let count = 0;
 function rightArrow(){
-    if(count<total-1){
+    if(count<total){
         slide.style.transition = 'transform 0.4s ease-in-out' ;
         count++;
         slide.style.transform = 'translatex('+(-size*count)+'px)';
-
-    }
- 
+    } 
 }
 function leftArrow(){
     if(count>0){
@@ -17,4 +17,4 @@ function leftArrow(){
         count--;
         slide.style.transform = 'translatex('+(-size*count)+'px)';
     }
-   }
+}
