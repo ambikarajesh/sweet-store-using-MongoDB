78f427f06d3003466cc32092515180ba80dc38a4
ambikarajesh <ambikula@gmail.com>
2019-03-06 17:25:45 -0800

    added left and right arrow for list item

---
+++ public/js/home.js
@@ -0,0 +1,12 @@
+
+// function rightArrow(){    
+//     const nav = document.querySelector('.main-content_nav')
+//     const itemList = document.querySelector('.main-content_item-list');
+//     console.log(itemList.offsetLeft)
+//     console.log(itemList.offsetWidth)
+//     console.log(nav.offsetWidth)
+//     itemList.style.left = '-1000px';
+//  }
+//  function leftArrow(){
+//      console.log('left')
+//  }
